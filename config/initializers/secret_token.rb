051361bc32f4bd542096319bf5d49bec2174bcd3
bernhard.w <bernhard.w@gms.info>
2013-10-12 16:18:01 +0200

    initiale commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestPuma::Application.config.secret_token = '590c3d55b234540ff56ef5741001d04ef8a6e0fbf47c96cdee1ee7b54394089b0e8d09d254420e8c0b19f860a64495d0e8630ee295c97973af6379fa35a44a80'
